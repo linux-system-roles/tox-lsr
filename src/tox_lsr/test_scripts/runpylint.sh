bbac320405deb78ec987b537903e5abe744f283e
Rich Megginson <rmeggins@redhat.com>
2020-12-08 10:39:37 -0700

    make coveralls work
    Add a custom coveralls script to work around the problems with
    coveralls and github actions.
    
    Add a `coveralls26` testenv so that we can use coveralls with
    py26.  This uses coveralls 1.x and coverage 4.x.
    
    When running the github action, only use the tox environment corresponding
    to the current system version of python installed by the github
    action.  For example, if github action has installed the python v3.6,
    we should only test py36-tox30, we should not test py27, py37, etc.
    other python environments.
    
    Get rid of references to Travis
    
    Fix bug with iniconfig used with tox 3.20 and later.

--- src/tox_lsr/test_scripts/runpylint.sh
+++ src/tox_lsr/test_scripts/runpylint.sh
@@ -5,11 +5,6 @@
 # set environment variables defined in config.sh before custom_pylint.py
 # invocation, so user can control what should be pylinted via config.sh.
 
-# Note: Prior this change, RUN_PYLINT_* environment variables set in config.sh
-#       take effect only when running inside Travis (because of runtox script
-#       which set them by including config.sh). Now, they take effect also when
-#       running tox locally.
-
 # The given command line arguments are passed to custom_pylint.py.
 
 # Environment variables:
