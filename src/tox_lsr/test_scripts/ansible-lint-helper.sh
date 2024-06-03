0d810a9a61a2e620d599296f09621073dfa6a321
Rich Megginson <rmeggins@redhat.com>
2024-06-03 08:53:54 -0600

    do not touch yamllint.yml in ansible-lint helper

--- src/tox_lsr/test_scripts/ansible-lint-helper.sh
+++ src/tox_lsr/test_scripts/ansible-lint-helper.sh
@@ -9,8 +9,6 @@ SCRIPTDIR=$(readlink -f "$(dirname "$0")")
 . "${SCRIPTDIR}/utils.sh"
 
 role=$(basename "${TOPDIR}")
-yl=.yamllint.yml
-yl_bkup="$LSR_TOX_ENV_TMP_DIR/.yamllint.yml.backup"
 mm=meta/main.yml
 mm_bkup="$LSR_TOX_ENV_TMP_DIR/meta_main.yml.backup"
 
@@ -25,21 +23,11 @@ pre() {
             sed "/galaxy_info:/a\  role_name: $role" -i "$mm"
         fi
     fi
-    if [ -f "$yl" ]; then
-        cp -p "$yl" "$yl_bkup"
-        if grep -q "^extends: yamllint_defaults.yml$" "$yl"; then
-            sed "/^extends: yamllint_defaults.yml$/s,yamllint_defaults.yml,${LSR_CONFIGDIR}/yamllint_defaults.yml," \
-                -i "$yl"
-        fi
-    fi
 }
 
 post() {
     if [ -f "$mm_bkup" ]; then
         mv "$mm_bkup" "$mm"
-    fi
-    if [ -f "$yl_bkup" ]; then
-        mv "$yl_bkup" "$yl"
     fi
 }
 
